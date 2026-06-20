5b95ac93ebc32f1698c233c83ad959a811970492
xlavelia <xlavelia@mail.ru>
2026-06-20 14:28:15 +0400

    Total remake design, only hsl(61, 85%, 78%), (home, trade, order, history, home lend)

--- Home.jsx
+++ Home.jsx
@@ -6,7 +6,8 @@ import { useBalance, usePositions, useTradeHistory, useProfile, useTransfers } f
 // npx vite --host 0.0.0.0 --port 5173 --force
 // git add .
 //  git commit -m "Total remake design, only hsl(61, 85%, 78%), (home, trade, order, history, home lend) "
-// git push origin main
+// git push main
+
 
 
 const Home = () => {
